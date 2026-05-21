368bd830760bd645ffdcb69a53a23d2fe7bcad93
Mattortx <mor.racing99@gmail.com>
2026-05-22 00:40:38 +0200

    fix: añadir PATCH y DELETE a CORS methods

--- server/index.js
+++ server/index.js
@@ -121,7 +121,7 @@ function sendJson(response, status, payload) {
 
 function applyCors(response) {
   response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  response.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
   response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 }
 
